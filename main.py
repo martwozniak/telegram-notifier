7afb2b508e677d5c3cb5e390b2b802e9790e707f
martwozniak <persky03@gmail.com>
2022-11-19 19:44:06 +0100

    Working notifier with .env variables

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+
+# Main function of notifier
 def send_telegram_message(message: str,
                           chat_id: str,
                           api_key: str,
@@ -15,17 +20,29 @@ def send_telegram_message(message: str,
             'https': f'http://{username}:{password}@{proxy_url}',
             'http': f'http://{username}:{password}@{proxy_url}'
         }
-        headers = {'Content-Type': 'application/json',
-                   'Proxy-Authorization': 'Basic base64'}
-        data_dict = {'chat_id': chat_id,
-                     'text': message,
-                     'parse_mode': 'HTML',
-                     'disable_notification': True}
-        data = json.dumps(data_dict)
-        url = f'https://api.telegram.org/bot{api_key}/sendMessage'
-        response = requests.post(url,
-                                 data=data,
-                                 headers=headers,
-                                 proxies=proxies,
-                                 verify=False)
-        return response
+    headers = {'Content-Type': 'application/json',
+                'Proxy-Authorization': 'Basic base64'}
+    data_dict = {'chat_id': chat_id,
+                    'text': message,
+                    'parse_mode': 'HTML',
+                    'disable_notification': True}
+    data = json.dumps(data_dict)
+    url = f'https://api.telegram.org/bot{api_key}/sendMessage'
+    response = requests.post(url,
+                                data=data,
+                                headers=headers,
+                                proxies=proxies,
+                                verify=False)
+    print(response) # 4 Debugging not important
+    return response
+
+
+# Get Secret Keys from .env file 
+# Remember to create .env base on .env.example
+chat_id = os.getenv('chat_id')
+api_key = os.getenv('api_key')
+
+
+# Call method if error occurs, then notification is pushed to telegram
+send_telegram_message("Site is down! You have to check it now.", chat_id, api_key)
+
