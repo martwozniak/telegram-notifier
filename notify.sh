f7fc3b91f5706a7557f2bf5782734473681e372a
martwozniak <persky03@gmail.com>
2022-11-19 19:47:32 +0100

    Script [.sh] and TODO

---
+++ notify.sh
@@ -0,0 +1,11 @@
+# Call notifier script
+# TODO: Add messege as an argument to this script like python3 main.py "Site is down", python3 main.py "Disk space is running low"
+python3 main.py
+
+
+
+# Remember to give this script permissions
+# chmod +x notify.sh
+
+# Calling:
+# ./notify.sh
